5258e11c4034e3369c23b5b637826bf8b0ec1bbc
William Wingate <wtwingate@gmail.com>
2024-05-13 23:00:51 -0400

    Add custom handler and readiness endpoint

--- main.go
+++ main.go
@@ -10,7 +10,8 @@ func main() {
 	const port = "8080"
 
 	mux := http.NewServeMux()
-	mux.Handle("/", http.FileServer(http.Dir(root)))
+	mux.Handle("/app/*", http.StripPrefix("/app", http.FileServer(http.Dir(root))))
+	mux.HandleFunc("/healthz", handlerReadiness)
 
 	srv := &http.Server{
 		Addr:    ":" + port,
@@ -19,4 +20,10 @@ func main() {
 
 	log.Printf("Serving files from %s on port: %s\n", root, port)
 	log.Fatal(srv.ListenAndServe())
+}
+
+func handlerReadiness(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "text/plan; charset=utf-8")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(http.StatusText(http.StatusOK)))
 }
